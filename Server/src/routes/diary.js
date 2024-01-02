c0d6a4f0ba29afc0ff81f04b816c1cfb8e7a2761
EVELO <evelo0702@gmail.com>
2024-01-02 21:43:51 +0900

    feat server get / delete / update

--- Server/src/routes/diary.js
+++ Server/src/routes/diary.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const Diary = require("../schemas/Diary");
+const { ObjectId } = require("mongodb");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   const diary = await Diary.find({ author: req.query.author });
+  return res.status(200).json(diary);
+});
+router.get("/detail", async (req, res) => {
+  const diary = await Diary.find({ _id: new ObjectId(req.query.id) });
   return res.status(200).json(diary);
 });
 router.post("/", async (req, res) => {
@@ -21,6 +26,21 @@ router.post("/", async (req, res) => {
       commit: newNote.commit,
     });
     res.status(200).send("전송완료");
+  } catch (e) {
+    console.log(e);
+  }
+});
+router.delete("/delete", async (req, res) => {
+  try {
+    await Diary.deleteOne({ _id: new ObjectId(req.query.id) });
+    res.status(200).send("삭제완료");
+  } catch (e) {
+    console.log(e);
+  }
+});
+router.post("/update", async (req, res) => {
+  try {
+    await Diary.updateOne({ _id: new ObjectId(req.query.id) }, { $set: {} });
   } catch (e) {
     console.log(e);
   }
