5e6412ab3bca026aad2b9cf2e1b5abe666da3ccc
EVELO <evelo0702@gmail.com>
2024-12-12 02:56:53 +0900

    update: 성능개선

--- Front/tailwind.config.js
+++ Front/tailwind.config.js
@@ -7,12 +7,11 @@ export default {
     extend: {
       fontFamily: {
         Dongle: ['"Dongle"', "sans-serif"],
-        Poppins: ['"Poppins"', "sans-serif"],
-        NanumMyeongjo: ['"Nanum Myeongjo"', "sans-serif"],
       },
       boxShadow: {
         cus: "0px 7px 29px 0px rgba(100,100,111,0.2)",
       },
+      fontDisplay: "swap",
     },
   },
   plugins: [typograyphy],
